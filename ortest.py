333cacf4da3ed0c0b1e72bed14155f987d0b97a2
AldoEHF14 <aldo.hernandez@cua.uam.mx>
2025-06-02 12:13:02 -0600

    Proyecto de la UEA de Computacion Evolutiva

---
+++ ortest.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def crear(nombre):
+
+    archivo = open(nombre, "r")
+    #sizes[0] = cantidad de puntos, sizes[1] = cantidad de conexiones
+    sizes = list(map(int, archivo.readline().split()))
+    #Genera matriz con ceros
+    matriz = []
+    for i in range(sizes[0]):
+        matriz.append([])
+        for j in range(sizes[0]):
+            matriz[i].append(0)
+
+    for i in range(sizes[1]):
+        linea = list(map(int, archivo.readline().split()))
+        matriz[linea[0] - 1][linea[1] - 1] = linea[2]
+        matriz[linea[1] - 1][linea[0] - 1] = linea[2]
+
+    npuntos = int(archivo.readline())
+    puntos = list(map(int, archivo.readline().split()))
+
+    archivo.close()
+
+    return matriz, puntos, sizes[0]
+
+matriz, puntos, size = crear("steinb2.txt")
+G = nx.from_numpy_array(np.array(matriz))
+layout = nx.spring_layout(G)
+vertex_colors = ['blue' if not vertex in puntos else 'green' for vertex in G.nodes()]
+nx.draw(G, layout, with_labels=True, node_color=vertex_colors)
+labels = nx.get_edge_attributes(G, 'weight')
+nx.draw_networkx_edge_labels(G, pos=layout, edge_labels=labels)
+plt.show()
